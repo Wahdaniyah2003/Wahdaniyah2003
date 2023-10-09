8c7993e9f4c16382638873351260225a44284c87
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-09 19:30:17 +0800

    day1.tipedata

---
+++ day1.tipedata.java
@@ -0,0 +1,28 @@
+
+public class day1 {
+    public static void main(String[] args) {
+        String nama = "Wahdaniyah";
+        String alamat = "Tapalang";
+        int umur = 20;
+        String nim = "D0221038";
+        char kelas = 'C';
+        String prodi = "Informatika";
+        long angkatan = 2021;
+        float berat = 51;
+        double tinggi = 156;
+        boolean belum_menikah = true;
+        System.out.println("Nama          : " + nama);
+        System.out.println("Alamat        : " + alamat);
+        System.out.println("Umur          : " + umur + " tahun");
+        System.out.println("Nim           : " + nim);
+        System.out.println("Kelas         : " + kelas);
+        System.out.println("Prodi         : " + prodi);
+        System.out.println("Angkatan      : " + angkatan);
+        System.out.println("Berat         : " + berat);
+        System.out.println("Tinggi        : " + tinggi);
+        System.out.println("Belum Menikah : " + belum_menikah);
+    }
+    
+}
+      
+
