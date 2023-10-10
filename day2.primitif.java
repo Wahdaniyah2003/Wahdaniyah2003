c0e7d11b14044414f297d172ec284f859f69772d
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-10 18:07:02 +0800

    day2.primitif

---
+++ day2.primitif.java
@@ -0,0 +1,26 @@
+
+public class day2 {
+    public static void main(String[] args) {
+        //Tipe data primitif adalah tipe data dasar yang sudah tersedia dalam bahasa pemrograman.
+        //sedangkan tipe data non-primitif adalah objek yang memiliki metode tambahan untuk berinteraksi dengannya.
+        
+        //Tipe data primitif
+        int umur = 20; // Ini adalah tipe data primitif untuk angka bulat
+        boolean  mahasiswa = true; // Ini adalah tipe data primitif untuk nilai boolean false dan true
+        double beratBadan = 55; // ini adalah tipe data double untuk bilangan pecahan
+        char golonganDarah = 'B'; // Ini adalah tipe data primitif untuk karakter
+        
+        // Menampilkan nilai dari tipe data primitif
+        System.out.println("Tipe Data Primitif:");
+        System.out.println("Umur          : "+ umur);
+        System.out.println("mahasiswa     : "+mahasiswa);
+        System.out.println("Berat Badan   : "+beratBadan);
+        System.out.println("Golongan darah: "+golonganDarah);
+        System.out.println("-----------------------------------------");
+        
+        // Tipe data non-primitif (objek)
+        int nilai = 100;//Ini adalah contoh variabel dengan tipe data non-primitif String
+        System.out.println("Nilai : "+nilai);//Ini untuk menampilkan karakter tipe data primitif
+       
+    }
+}
