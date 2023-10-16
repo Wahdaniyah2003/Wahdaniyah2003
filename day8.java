6326da1d13e23a86ae29b46daaa95a23e5be0445
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-16 19:47:27 +0800

    day8.java

---
+++ day8.java
@@ -0,0 +1,13 @@
+
+public class day8 {
+    public static void main(String[] args) {
+        int angka = 10;
+        
+        if(angka > 5){
+            System.out.println("Angka  Lebih dari  5");
+        }else{
+            System.out.println("Angka Tidak lebih besar dari 5");
+        }
+            
+    }
+}
