c9c5f538de817e4c7ecd90db6a682f187885575a
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-17 20:22:39 +0800

    day9_switchcase

---
+++ day9_switch case.java
@@ -0,0 +1,23 @@
+
+public class day9 {
+    public static void main(String[] args) {
+            String pesan = "tiga";
+            switch(pesan)
+            {
+                case "satu":
+                    System.out.println("Selamat");
+                    break;
+                case "dua":
+                    System.out.println("Belajar");
+                    break;
+                case "tiga":
+                    System.out.println("Java");
+                    break;
+                default:
+                    System.out.println("tidak ditemukan");
+                
+            }
+        }
+        
+    }
+
