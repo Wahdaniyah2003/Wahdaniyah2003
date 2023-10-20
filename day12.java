b67b2cbef7440e27c800ffdf53f63ad420101606
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-20 23:02:18 +0800

    day12.java

---
+++ day12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class day12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            for(int i = 0; i < 10; i++){
+                System.out.println("Selamat Belajar");
+        }
+    }
+}
