9fc368287ca63e23fc170cd373f1f0de2493c051
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-21 20:49:32 +0800

    day13.java

---
+++ day13.java
@@ -0,0 +1,9 @@
+
+public class day13 {
+    public static void main(String[] args) {
+        
+        for(int i =1; i <= 200; i++){
+            System.out.println("Tetap Semangat Digempuran Tugas");
+        }
+    }
+}
