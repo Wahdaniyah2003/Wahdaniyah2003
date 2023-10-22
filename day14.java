a9d269af6bf96b641205f937464bfcc7757e5735
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-22 22:41:34 +0800

    day14

---
+++ day14.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class day14 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            for(int i = 0; i <= 8; i++){
+                System.out.println("Semangat terus");
+            }
+            System.out.println("=========================================");
+            
+            for(int i = 1; i <= 12; i++){
+                System.out.println("Jgn Pantang Menyerah");
+            }
+        }
+    }
+
+
+
