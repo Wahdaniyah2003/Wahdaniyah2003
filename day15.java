a9ad9525ec18e8beba1c80e0d1ff2e568a37218b
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-23 22:41:07 +0800

    day15

---
+++ day15.java
@@ -0,0 +1,19 @@
+
+public class day15 {
+    public static void main(String[] args) {
+        //Belajar Tipe data primitif
+        
+        int umur = 20;
+        System.out.println("Umur Saya Adalah : "+ umur);
+       
+        System.out.println("===========================");
+        
+        char jenisKelamin = 'P';
+        System.out.println("jenis Kelamin :" + jenisKelamin);
+        
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        
+        boolean SudahPunyaPacar = true;
+        System.out.println("Sudah Punya pacar ? " + SudahPunyaPacar);
+    }
+}
