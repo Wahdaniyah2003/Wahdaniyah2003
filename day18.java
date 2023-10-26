a3acb9869bae23d7b205d988c302a1edf180e7bd
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-26 19:36:52 +0800

    day18

---
+++ day18.java
@@ -0,0 +1,12 @@
+
+public class day18 {
+    public static void main(String[] args) {
+        //Menghitung jumlah dua angka
+        
+       int num1 = 10;
+       int num2 = 12;
+       int sum = num1 + num2;
+        System.out.println("Hasil Jumlah: " + sum);
+    }
+}
+
