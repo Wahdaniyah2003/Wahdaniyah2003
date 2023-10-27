cb5521c4e57fc7932158e08f7f825e430539e1ce
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-10-27 19:12:18 +0800

    day19

---
+++ day19.java
@@ -0,0 +1,12 @@
+
+public class day19 {
+    public static void main(String[] args) {
+        int j = 0; 
+        
+        while(j < 8){
+            System.out.println("Ini adalah Nilai j :" + j);
+            j++;
+        }
+        
+    }
+}
