77a71d30da18f586c4ce2dfd73b8dd36a093971d
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-11-01 20:37:54 +0800

    day24

---
+++ day24.java
@@ -0,0 +1,20 @@
+
+public class day24 {
+    public static void main(String[] args) {
+        int Pilihan = 4;
+        
+        switch(Pilihan){
+            case 1:
+                System.out.println("Saya Memilih Opsi 1");
+                break;
+            case 2:
+                System.out.println("Saya Memilih Opsi 2");
+                break;
+            case 3:
+                System.out.println("Saya Memilih opsi 3");
+                break;
+            default:
+                System.out.println("Pilihan Tidak Valid");
+        }
+    }
+}
