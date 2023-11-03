735290baec7564b3f62fbcbb1950f09ca605dc3b
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-11-03 21:45:30 +0800

    day26

---
+++ day26.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class day26 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            for(int i = 1; i <= 100; i++){
+                System.out.println("Selamat Anda Lulus");
+            }
+    }
+}
