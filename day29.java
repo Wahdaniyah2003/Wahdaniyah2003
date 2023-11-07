4657b6caf24a95bde82f2fa46c15020d6aa108b3
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-11-07 12:21:56 +0800

    Update day29.java

--- day29.java
+++ day29.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class day29 {
     public static void main(String[] args) {
         //Membuat inputan angka yang outputnya menampilkan jumlah angka yang habis dibagi tiga
-       Scanner scn = new Scanner(System.in);   
+        Scanner scn = new Scanner(System.in);   
         System.out.println("Masukkan angka : "); 
         int angka  =  scn.nextInt();
         for (int i = 1; i <= angka; i++) {
