4fa5a1ac59c1aae6657678b3a92c7f6c3c0691ae
Wahdaniyah2003 <136802520+Wahdaniyah2003@users.noreply.github.com>
2023-11-09 15:17:35 +0800

    day32

---
+++ day32.java
@@ -0,0 +1,13 @@
+
+public class day32 {
+    public static void main(String[] args) {
+        // perulangan menggunakan do-while
+        int a = 3; // 3 buah bilangan positif pertama
+        int i = 1;
+        do
+        {
+            System.out.println(i);
+        }
+        while (i < a);
+    }
+}
